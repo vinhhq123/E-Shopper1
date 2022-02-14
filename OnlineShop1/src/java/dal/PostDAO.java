43b0fce59f2cf631742156dde15eddd19f0750b2
hungng1301 <90265514+hungng1301@users.noreply.github.com>
2022-02-15 02:31:31 +0700

    commit update blog category

--- OnlineShop1/src/java/dal/PostDAO.java
+++ OnlineShop1/src/java/dal/PostDAO.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 import model.PostList;
+import model.Setting;
 import model.User;
 
 /**
@@ -25,15 +26,16 @@ import model.User;
  * @author CHANHSIRO
  */
 public class PostDAO extends DBContext {
-    
-    
+
     Connection connection = null;
     PreparedStatement ps = null;
     ResultSet result = null;
 
     public List<PostList> getBlogSortByDate() {
         List<PostList> list = new ArrayList<>();
-        String query = "select p.*, u.uid, u.fullname from post p inner join user u on p.postAuthor = u.uid order by p.postdDate desc";
+        String query = "select p.* , u.uid, u.fullname, s.settingId, s.settingValue from post p \n"
+                + "inner join user u on p.postAuthor = u.uid\n"
+                + "inner join setting s on p.postCategory = s.settingId";
         try {
             connection = new DBContext().getConnection();
             ps = connection.prepareStatement(query);
@@ -47,7 +49,8 @@ public class PostDAO extends DBContext {
                 p.setPostContent(result.getString("postContent"));
                 User u = new User(result.getInt("uid"), result.getString("fullname"));
                 p.setPostAuthor(u);
-                p.setCategory(result.getString("postCategory"));
+                Setting s = new Setting(result.getInt("settingId"), result.getString("settingValue"));
+                p.setCategory(s);
                 p.setFeatured(result.getString("featured"));
                 p.setSatatusPL(result.getInt("status"));
                 p.setUpdateDate(result.getDate("postdDate"));
@@ -80,7 +83,10 @@ public class PostDAO extends DBContext {
     }
 
     public PostList getBlogById(int id) {
-        String query = "select p.*, u.uid, u.fullname from post p inner join user u on p.postAuthor = u.uid where p.postId = ?";
+        String query = "select p.* , u.uid, u.fullname, s.settingId, s.settingValue from post p \n"
+                + "inner join user u on p.postAuthor = u.uid\n"
+                + "inner join setting s on p.postCategory = s.settingId\n"
+                + "where p.postId = ?";
         try {
             connection = new DBContext().getConnection();
             ps = connection.prepareStatement(query);
@@ -95,7 +101,8 @@ public class PostDAO extends DBContext {
                 p.setPostContent(result.getString("postContent"));
                 User u = new User(result.getInt("uid"), result.getString("fullname"));
                 p.setPostAuthor(u);
-                p.setCategory(result.getString("postCategory"));
+                Setting s = new Setting(result.getInt("settingId"), result.getString("settingValue"));
+                p.setCategory(s);
                 p.setFeatured(result.getString("featured"));
                 p.setSatatusPL(result.getInt("status"));
                 p.setUpdateDate(result.getDate("postdDate"));
@@ -127,16 +134,19 @@ public class PostDAO extends DBContext {
         return null;
     }
 
-    public List<PostList> getBlogCategory() {
-        List<PostList> listPostCate = new ArrayList<>();
-        String query = "SELECT postCategory FROM post";
+    public List<Setting> getBlogCategory() {
+        List<Setting> listPostCate = new ArrayList<>();
+        String query = "select settingId, settingValue from setting where settingId = 8 or settingId = 9";
         try {
             connection = new DBContext().getConnection();
             ps = connection.prepareStatement(query);
             result = ps.executeQuery();
 
             while (result.next()) {
-                listPostCate.add(new PostList(result.getString(1)));
+               // PostList p = new PostList();
+                Setting s = new Setting(result.getInt("settingId"), result.getString("settingValue"));
+                
+                listPostCate.add(s);
             }
         } catch (Exception e) {
 
@@ -146,7 +156,10 @@ public class PostDAO extends DBContext {
 
     public List<PostList> getBlogByPostCategory(String id) {
         List<PostList> list = new ArrayList<>();
-        String query = "select p.* , u.uid, u.fullname from post p inner join user u on p.postAuthor = u.uid where p.postCategory = ?";
+        String query = "select p.* , u.uid, u.fullname, s.settingId, s.settingValue from post p \n"
+                + "inner join user u on p.postAuthor = u.uid\n"
+                + "inner join setting s on p.postCategory = s.settingId\n"
+                + "where p.postCategory = ?";
         try {
             connection = new DBContext().getConnection();
             ps = connection.prepareStatement(query);
@@ -162,7 +175,8 @@ public class PostDAO extends DBContext {
                 p.setPostContent(result.getString("postContent"));
                 User u = new User(result.getInt("uid"), result.getString("fullname"));
                 p.setPostAuthor(u);
-                p.setCategory(result.getString("postCategory"));
+                Setting s = new Setting(result.getInt("settingId"), result.getString("settingValue"));
+                p.setCategory(s);
                 p.setFeatured(result.getString("featured"));
                 p.setSatatusPL(result.getInt("status"));
                 p.setUpdateDate(result.getDate("postdDate"));
@@ -176,7 +190,10 @@ public class PostDAO extends DBContext {
 
     public List<PostList> searchBlogByTitle(String search) {
         List<PostList> list = new ArrayList<>();
-        String query = "select p.* , u.uid, u.fullname from post p inner join user u on p.postAuthor = u.uid where p.postTitle like ?";
+        String query = "select p.* , u.uid, u.fullname, s.settingId, s.settingValue from post p \n"
+                + "inner join user u on p.postAuthor = u.uid\n"
+                + "inner join setting s on p.postCategory = s.settingId\n"
+                + "where p.postTitle like ?";
         try {
             connection = new DBContext().getConnection();
             ps = connection.prepareStatement(query);
@@ -191,7 +208,8 @@ public class PostDAO extends DBContext {
                 p.setPostContent(result.getString("postContent"));
                 User u = new User(result.getInt("uid"), result.getString("fullname"));
                 p.setPostAuthor(u);
-                p.setCategory(result.getString("postCategory"));
+                Setting s = new Setting(result.getInt("settingId"), result.getString("settingValue"));
+                p.setCategory(s);
                 p.setFeatured(result.getString("featured"));
                 p.setSatatusPL(result.getInt("status"));
                 p.setUpdateDate(result.getDate("postdDate"));
@@ -231,11 +249,11 @@ public class PostDAO extends DBContext {
         }
         return arr;
     }
-    
+
     public void insertPost(String postContent, InputStream thumbnail, String postTitle, int postAuthor) {
-        
-        String sql = "INSERT INTO `post` (`thumbnail`, `postTitle`, `postContent`, `postAuthor`) \n" +
-"        VALUES (?, ?, ?, ?)";
+
+        String sql = "INSERT INTO `post` (`thumbnail`, `postTitle`, `postContent`, `postAuthor`) \n"
+                + "        VALUES (?, ?, ?, ?)";
         try {
             connection = getConnection();
             ps = connection.prepareStatement(sql);
@@ -256,20 +274,20 @@ public class PostDAO extends DBContext {
                 System.out.println("Exception ==== " + ex);
             }
         }
-        
+
     }
-    
+
     public ArrayList<PostList> getPostList() throws Exception {
-            ArrayList<PostList> postlist = new ArrayList<>();
+        ArrayList<PostList> postlist = new ArrayList<>();
         try {
-        String sql = "select  p.thumbnail, p.postTitle, p.postContent, u.fullname from post as p\n" +
-                        "Inner Join user as u ON u.uid = p.postAuthor";
-        System.out.println(sql);
-        String noImage = "";
+            String sql = "select  p.thumbnail, p.postTitle, p.postContent, u.fullname from post as p\n"
+                    + "Inner Join user as u ON u.uid = p.postAuthor";
+            System.out.println(sql);
+            String noImage = "";
             connection = getConnection();
             ps = connection.prepareStatement(sql);
             result = ps.executeQuery();
-            
+
             while (result.next()) {
                 PostList post = new PostList();
                 post.setPostContent(result.getString("postContent"));
@@ -313,6 +331,14 @@ public class PostDAO extends DBContext {
         }
         return postlist;
     }
-}
-    
 
+    public static void main(String[] args) {
+
+        PostDAO dao = new PostDAO();
+        PostList postList = dao.getBlogById(1);
+//        List<PostList> list = dao.getBlogByPostCategory("1");
+//        for (PostList postList : list) {
+            System.out.println(postList);
+//        }
+    }
+}
